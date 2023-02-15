896ad808599286b9d092730d7c8ea53d08472980
Airenik Melkonyan <swenovish@gmail.com>
2023-02-15 22:28:17 +0300

    Бумага, камень, ножницы

---
+++ services/services.py
@@ -0,0 +1,28 @@
+from random import choice
+
+from lexicon.lexicon_ru import LEXICON_RU
+
+def get_bot_choice() -> str:
+    return choice(['rock', 'scissors', 'paper'])
+
+
+def _normalize_user_answer(user_answer: str) -> str:
+    for key in LEXICON_RU:
+        if LEXICON_RU[key] == user_answer:
+            return key
+    raise Exception
+
+
+def get_winner(user_choice: str, bot_choice: str) -> str:
+    user_choice: str = _normalize_user_answer(user_choice)
+    rules: dict[str, str] = {
+                             'rock': 'scissors',
+                             'scissors': 'paper',
+                             'paper': 'rock'    
+                            }
+    
+    if user_choice == bot_choice:
+        return 'nobody_won'
+    elif rules[user_choice] == bot_choice:
+        return 'user_won'
+    return 'bot_won'
